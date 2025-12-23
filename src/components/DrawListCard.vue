5f06211a52264625ee06f67ed780b96e1478e020
Tun_wq <3321176187@qq.com>
2025-12-23 10:16:30 +0800

    drawlistcard移动端适配完成

--- src/components/DrawListCard.vue
+++ src/components/DrawListCard.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="card" id="draw-list-card" ref="drawListCard" @wheel="handleWheel" @mouseenter="showButtons = true" @mouseleave="showButtons = false"
-    @touchstart="handleTouchStart" @touchend="handleTouchEnd">
-    <div v-for="item in items" :key="item.id" class="item" :style="getItemStyle(item.id)">
-    </div>
-    <button class="nav-button nav-button-left" :class="{ 'show': showButtons || isMobile }" @click="movePrev">
+  <div
+    class="card"
+    id="draw-list-card"
+    ref="drawListCard"
+    @wheel="handleWheel"
+    @mouseenter="showButtons = true"
+    @mouseleave="showButtons = false"
+    @touchstart="handleTouchStart"
+    @touchend="handleTouchEnd"
+  >
+    <div v-for="item in items" :key="item.id" class="item" :style="getItemStyle(item.id)"></div>
+    <button
+      class="nav-button nav-button-left"
+      :class="{ show: showButtons || isMobile }"
+      @click="movePrev"
+    >
       <span class="arrow">‹</span>
     </button>
-    <button class="nav-button nav-button-right" :class="{ 'show': showButtons || isMobile }" @click="moveNext">
+    <button
+      class="nav-button nav-button-right"
+      :class="{ show: showButtons || isMobile }"
+      @click="moveNext"
+    >
       <span class="arrow">›</span>
     </button>
   </div>
@@ -27,7 +42,7 @@ export default {
   },
   methods: {
     getItemStyle(id) {
-      const imageUrl = `src/assets/image/home/pic${id+1}.jpg`;
+      const imageUrl = `src/assets/image/home/pic${id + 1}.jpg`
       return {
         backgroundImage: `url(${imageUrl})`,
         backgroundSize: 'cover',
@@ -36,39 +51,39 @@ export default {
 
     handleWheel(event) {
       if (event.deltaY > 0) {
-        this.moveNext();
+        this.moveNext()
       } else {
-        this.movePrev();
+        this.movePrev()
       }
-      event.preventDefault(); // 防止页面滚动
+      event.preventDefault() // 防止页面滚动
     },
     moveNext() {
-      const firstItem = this.items.shift();
-      this.items.push(firstItem);
+      const firstItem = this.items.shift()
+      this.items.push(firstItem)
     },
     movePrev() {
-      const lastItem = this.items.pop();
-      this.items.unshift(lastItem);
+      const lastItem = this.items.pop()
+      this.items.unshift(lastItem)
     },
     handleTouchStart(event) {
-      this.touchStartX = event.touches[0].clientX;
+      this.touchStartX = event.touches[0].clientX
     },
     handleTouchEnd(event) {
-      const touchEndX = event.changedTouches[0].clientX;
-      const diff = this.touchStartX - touchEndX;
+      const touchEndX = event.changedTouches[0].clientX
+      const diff = this.touchStartX - touchEndX
 
       // 如果滑动距离超过50px，则触发切换
       if (Math.abs(diff) > 50) {
         if (diff > 0) {
           // 左滑，对应鼠标滚轮下移，显示下一张
-          this.moveNext();
+          this.moveNext()
         } else {
           // 右滑，对应鼠标滚轮上移，显示上一张
-          this.movePrev();
+          this.movePrev()
         }
       }
     },
-  }
+  },
 }
 </script>
 
